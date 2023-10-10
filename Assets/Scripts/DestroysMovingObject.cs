96991982b4a0f762402710fea815961cd98f56cf
ghost <kbinas2022@elam.co.uk>
2023-10-10 17:05:21 +0100

    sound effect

--- Assets/Scripts/DestroysMovingObject.cs
+++ Assets/Scripts/DestroysMovingObject.cs
@@ -5,19 +5,24 @@ using System.Collections;
 public class DestroysMovingObject : MonoBehaviour
 {
     public AudioSource src;
-    public AudioClip sfx1; 
+    public AudioClip sfx1;
+
+    public GameObject Src; 
+
+    
     // This function is called when a collision occurs.
     public void OnCollisionEnter(Collision collision)
     {
         // Check if the collision involves a specific tag or layer.
         if (collision.gameObject.CompareTag("DestroyableTag"))
         {
+            Src = GameObject.FindWithTag("DestroyableSound");
+
+            src = Src.GetComponent<AudioSource>();
+
+            src.Play(); 
             // Destroy the object that this script is attached to.
             Destroy(gameObject);
-
-            Debug.Log("Destroy Mit");
-            src.clip = sfx1;
-            src.Play(); 
         }
     }
 }
