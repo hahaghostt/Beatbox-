f721995f74665450af4fa1835291301f0a7a52bf
ghost <kbinas2022@elam.co.uk>
2023-10-19 15:40:18 +0100

    fixing punch mechanic

---
+++ Assets/Scripts/RED.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.XR;
+
+public class RED : MonoBehaviour
+{
+    public AudioSource src;
+    public AudioClip sfx1;
+
+    public GameObject Src;
+    public int pointsToAdd = 10;
+
+    [SerializeField] GameObject Firework;
+
+    /*
+    public GameObject Score;
+    public int theScore = 0;
+
+    public ScoreSystem score; 
+    */
+
+    public bool isPunching = false;
+    public Vector3 previousPosition;
+
+    void Update()
+    {
+        // Check for punch motion by calculating the velocity of the controller.
+        XRNode controllerNode = GetControllerNode(this.gameObject);
+        InputDevice device = InputDevices.GetDeviceAtXRNode(controllerNode);
+
+        Vector3 currentPosition;
+        if (device.TryGetFeatureValue(CommonUsages.devicePosition, out currentPosition))
+        {
+
+            float velocity = (currentPosition - previousPosition).magnitude / Time.deltaTime;
+
+
+            if (velocity > 2.0f)
+            {
+                isPunching = true;
+            }
+            else
+            {
+                isPunching = false;
+            }
+            previousPosition = currentPosition;
+        }
+    }
+
+
+
+    public void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("DestroyableTag4"))
+        {
+            /* score.AddScore(1); */
+            Src = GameObject.FindWithTag("DestroyableSound");
+
+            src = Src.GetComponent<AudioSource>();
+
+            XRNode controllerNode = GetControllerNode(this.gameObject);
+            InputDevice device = InputDevices.GetDeviceAtXRNode(controllerNode);
+            device.SendHapticImpulse(0, 0.5f, 0.1f);
+
+
+            ScoreManager scoreManager = GameObject.FindObjectOfType<ScoreManager>();
+            if (scoreManager != null)
+            {
+                scoreManager.IncreaseScore(pointsToAdd);
+            }
+
+            src.Play();
+
+            GameObject explosion = Instantiate(Firework, transform.position, Firework.transform.rotation);
+            /* Firework.Play(); */
+            Destroy(explosion, 0.75f);
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("DestroyableTag2"))
+        {
+            Destroy(gameObject);
+        }
+    }
+    private XRNode GetControllerNode(GameObject controller)
+    {
+        if (controller.name.Contains("Right"))
+        {
+            return XRNode.RightHand;
+        }
+        else if (controller.name.Contains("Left"))
+        {
+            return XRNode.LeftHand;
+        }
+        // Return the default value if not found
+        return XRNode.RightHand;
+    }
+}
