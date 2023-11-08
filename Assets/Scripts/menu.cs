618fdf68177bd0f45e237734376a2d1403d9a5a3
ghost <kbinas2022@elam.co.uk>
2023-11-08 10:45:42 +0000

    VOICE ACTING!

--- Assets/Scripts/menu.cs
+++ Assets/Scripts/menu.cs
@@ -6,11 +6,30 @@ public class menu : MonoBehaviour
     public AudioSource mouseclick;
     public AudioClip sfx3;
 
+    public AudioSource voiceacting;
+    public AudioClip sfx4;
+
+    [SerializeField] 
+    private float delayafterscene = 10f;
+
+    [SerializeField]
+    private float timeElapsed; 
+
     public void NextScene()
     {
         Time.timeScale = 1;
-        SceneManager.LoadScene("Level 1");
+        voiceacting.Play();
         mouseclick.Play();
+
+        timeElapsed += Time.deltaTime;
+    
+       if (timeElapsed > delayafterscene)
+       {
+            Debug.Log("Play Next Scene");
+            SceneManager.LoadScene("Level 1");
+        }
+            
+    
     }
 
     public void Credits()
